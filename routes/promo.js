6cdf130a5fad0baa54d6264117022a23bcbe0ef3
Ryuseikai <kthkun@gmail.com>
2025-11-06 14:57:00 +0700

    v

--- routes/promo.js
+++ routes/promo.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { auth } = require('../middleware/auth');
-const admin = require('../middleware/admin');
+const { admin } = require('../middleware/admin');
 const promoController = require('../controllers/promoController');
 
 // Apply promo code (User)
